cf518672cb37f7297d0a6d75ad41112f73efcb6e
EmperorKnight <jlpicado@uamv.edu.ni>
2025-04-22 19:51:31 -0600

    Ejercicio 9 completo

---
+++ Actividad_4/Ejercicio_9.py
@@ -0,0 +1,27 @@
+# Encontrar el mayor de tres números
+# • Ingresar el primer número
+# • Ingresar el segundo número
+# • Ingresar el tercer número
+# • Si el primero es mayor o igual que el segundo, comparar el primero con el tercero
+# • Si el primero es menor que el segundo, compara el segundo con el tercero
+# • Si los tres son iguales, mostrar “Los números son iguales”
+
+primer_numero = int(input(f"Introduzca el primer numero \n-> "))
+segundo_numero = int(input(f"Introduzca el segundo numero \n-> "))
+tercer_numero = int(input(f"Introduzca el tercer numero \n-> "))
+
+if primer_numero > segundo_numero:
+    uno = primer_numero > tercer_numero
+    if uno == True:
+        print("El primer numero es el mayor entre los tres")
+    else:
+        print("El tercer numero es el mayor entre los tres")
+elif primer_numero < segundo_numero:
+    uno = segundo_numero > tercer_numero
+    if uno == True:
+        print("El segundo numero es el mayor entre los tres")
+    else:
+        print("El tercer numero es el mayor entre los tres")
+
+if primer_numero == segundo_numero == tercer_numero:
+    print("Los tres numero son iguales")
