105a34ecc7538d59dd6694ca00a8568e5c2974d0
EmperorKnight <jlpicado@uamv.edu.ni>
2025-04-24 12:35:00 -0600

    Modifique el ejercicio 5 de la actividad 4

--- Actividad_4/Ejercicio_3.py
+++ Actividad_4/Ejercicio_3.py
@@ -3,9 +3,13 @@
 # • Si supera 120 km/h, mostrar “¡Reduzca la velocidad!”
 
 velocidad = input(f" \nIntroduzca su velocidad en (km/h)\n-> ")
-velocidad = int()
 
-if velocidad > 120:
+velocidad1, simbologia = velocidad.split(" ")
+
+velocidad2 = int(velocidad1)
+simbologia2 = str(simbologia)
+
+if velocidad2 > 120:
     print(f"¡Reduzca la velocidad!\n ")
 else:
     print(f"Siga su camino\n ")
